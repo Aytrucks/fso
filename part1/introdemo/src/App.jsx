07498c3b662aa00208e111b8fc4ec266fcf93555
Aytrucks WDWS <105306690+Aytrucks@users.noreply.github.com>
2025-01-07 16:21:12 -0700

    finish part1 c

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,8 +1,33 @@
-const App = () => {
-  console.log("Hello from component uwu")
+import { useState } from "react"
+
+const Display = ({counter}) => <div>{counter}</div>
+
+
+const Button = ({onSmash,text}) => <button onClick={onSmash}>{text}</button>
+
+
+const App = (props) => {
+  const [counter, setCounter ] = useState(-1)
+  console.log('rendering with counter value', counter)
+  const increaseBy1 = () => {
+    console.log('up counter val', counter)
+    setCounter(counter+1)
+  }
+  const decreaseBy1 = () => {
+    console.log('down counter val', counter)
+    setCounter(counter-1)
+  }
+  const set0 = () => {
+    console.log('zeroing', counter)
+    setCounter(0)
+  }
+
   return(
     <div>
-      <p>Hello World</p>
+      <Display counter={counter}/>
+      <Button onSmash={increaseBy1} text='ploos'/>
+      <Button onSmash={decreaseBy1} text='minoos'/>
+      <Button onSmash={set0} text='zeroo'/>
     </div>
   )
 }
