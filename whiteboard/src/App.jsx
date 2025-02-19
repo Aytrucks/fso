64f38ad784aa7bf82ef9ed4cb6780e2ad9b0ee0b
Aytrucks WDWS <105306690+Aytrucks@users.noreply.github.com>
2025-02-18 23:06:29 -0700

    complete lesson portion of 2b

--- whiteboard/src/App.jsx
+++ whiteboard/src/App.jsx
@@ -2,26 +2,45 @@ import { useState } from 'react'
 import Note from './components/Note'
 
 const App = (props) => {
+  console.log(props.notes)
+  //setX is a function that you pass the value of what you want X to be.
   const [notes,setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState("...hey bb,")
+  const [showAll, setShowAll] = useState(false)
 
   const addNote = (event) => {
+    //prevents page from reloading
     event.preventDefault();
-    console.log('button touched sensually',event.target)
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length+1),
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote("sweaty ballsacks")
   }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important)
   
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
         <Note note={note} key={note.id}/>
         )}
       </ul>
+
       <form onSubmit = {addNote}>
-        <input value={newNote}/>
+        <input value={newNote} onChange={handleNoteChange}/>
         <button type="submit">save</button>
       </form>
+
     </div>
   )
 }
