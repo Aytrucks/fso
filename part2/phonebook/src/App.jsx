a71f31e10427d3bc01cc9aed1b7c6991b07babd8
Aytrucks WDWS <105306690+Aytrucks@users.noreply.github.com>
2025-02-24 17:08:52 -0700

    fin add name

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,32 +1,56 @@
 import { useState } from 'react'
 
+const Person = ({dude}) => <div>{dude.name}</div>
+
 function App() {
+  const testStr = "Hello World"
   const [persons, setPersons] = useState([
     {name: "Jello Gray"}
   ])
   const [newname, setNewname] = useState("")
   const printName = (ppl) => {
-    console.log(ppl)
+    //console.log(ppl)
     const names = (ppl.map(dude => {
-      console.log(dude.name)
-      dude.name
+      return dude.name
     })
-    
-  )
+    )
+    //console.log(names)
+    console.log("3")
+    return names
   }
+
+  const addName = (event) => {
+    event.preventDefault()
+    console.log("Adding name")
+    const newMonkey = {
+      name: newname
+    }
+    console.log("1")
+    setPersons(persons.concat(newMonkey))
+    console.log("2")
+  }
+
+  const handleNewMonkey = (event) => {
+    console.log(event.target.value)
+    setNewname(event.target.value)
+  }
+
+  //console.log(persons.map(dude => dude.name))
   return(
     <div>
       <h2>Monkey Directory App</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          ur monkey name:<input/>
+          ur monkey name:<input value={newname} onChange={handleNewMonkey}/>
         </div>
         <div>
           <button type='submit'>add ape name</button>
         </div>
       </form>
       <h2>phone numbers of monkeys</h2>
-      {printName(persons)}
+      {persons.map((dude) => 
+        <Person dude={dude}/>
+      )}
     </div>
   )
 }
