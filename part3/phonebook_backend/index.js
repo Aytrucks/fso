b6778dd1799116e0da6b46c2f97f1cb81eb0217b
Aytrucks WDWS <105306690+Aytrucks@users.noreply.github.com>
2025-07-16 14:28:48 -0700

    linted

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,116 +1,116 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const app = express();
-const Entry = require("./models/entry");
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const Entry = require('./models/entry')
 
-app.use(express.static("dist"));
-app.use(express.json());
-morgan.token("body", (req) => {
-  return JSON.stringify(req.body);
-});
+app.use(express.static('dist'))
+app.use(express.json())
+morgan.token('body', (req) => {
+  return JSON.stringify(req.body)
+})
 
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
-);
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
-app.get("/api/health", (request, response) => {
-  response.send("Server is running!");
-});
+app.get('/api/health', (request, response) => {
+  response.send('Server is running!')
+})
 
-app.get("/info", (request, response, next) => {
+app.get('/info', (request, response, next) => {
   Entry.countDocuments({})
     .then((count) => {
-      const currentTime = new Date();
+      const currentTime = new Date()
       const info = {
         count: count,
         date: currentTime.toString(),
-      };
-      response.json(info);
+      }
+      response.json(info)
     })
-    .catch(next);
-});
+    .catch(next)
+})
 
-app.get("/api/people", (request, response) => {
+app.get('/api/people', (request, response) => {
   Entry.find({}).then((nums) => {
-    response.json(nums);
-  });
-});
+    response.json(nums)
+  })
+})
 
-app.get("/api/people/:id", (request, response, next) => {
+app.get('/api/people/:id', (request, response, next) => {
   Entry.findById(request.params.id)
     .then((num) => {
       if (num) {
-        response.json(num);
+        response.json(num)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
     .catch((error) => {
-      error = next(error);
-    });
-});
-
-app.delete("/api/people/:id", (request, response, next) => {
-  const id = request.params.id;
-  Entry.findByIdAndDelete(id)
-    .then((result) => {
-      response.status(204).end();
+      next(error)
     })
-    .catch((error) => next(error));
-});
+})
 
-app.put("/api/people/:id", (req, res, next) => {
-  const { name, number } = req.body;
+app.delete('/api/people/:id', (request, response, next) => {
+  const id = request.params.id
+  Entry.findByIdAndDelete(id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
+})
+
+app.put('/api/people/:id', (req, res, next) => {
+  const { name, number } = req.body
   Entry.findById(req.params.id)
     .then((entry) => {
       if (!entry) {
-        res.status(404).end();
+        res.status(404).end()
       }
-      entry.name = name;
-      entry.number = number;
+      entry.name = name
+      entry.number = number
       return entry.save().then((newEntry) => {
-        res.json(newEntry);
-      });
+        res.json(newEntry)
+      })
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.post("/api/people", (req, res, next) => {
-  const body = req.body;
+app.post('/api/people', (req, res, next) => {
+  const body = req.body
   if (!body.name || !body.number) {
     return res.status(400).json({
-      error: "missing name or number",
-    });
+      error: 'missing name or number',
+    })
   }
 
   const entry = new Entry({
     name: body.name,
     number: body.number,
-  });
+  })
 
   entry
     .save()
     .then((savedEntry) => {
-      res.json(savedEntry);
+      res.json(savedEntry)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
-  console.log(`Server running now on ${PORT}`);
-});
+  console.log(`Server running now on ${PORT}`)
+})
 
 const errorHandler = (error, req, res, next) => {
-  console.log(error.message);
-  if (error.name === "CastError") {
-    return res.status(400).send({ error: "messed up ID" });
+  console.log(error.message)
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'messed up ID' })
   }
-  if (error.name === "ValidationError") {
-    return res.status(400).json({ error: error.message });
+  if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   }
-  next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
--- part3/phonebook_backend/models/entry.js
+++ part3/phonebook_backend/models/entry.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-mongoose.set("strictQuery", false);
-console.log("Connecting to url");
+mongoose.set('strictQuery', false)
+console.log('Connecting to url')
 mongoose
   .connect(url)
-  .then((res) => {
-    console.log("Connected to the mongo");
+  .then(() => {
+    console.log('Connected to the mongo')
   })
   .catch((error) => {
-    console.log("Damn, better luck next time", error.message);
-  });
+    console.log('Damn, better luck next time', error.message)
+  })
 
 const entrySchema = new mongoose.Schema({
   name: {
@@ -22,20 +22,20 @@ const entrySchema = new mongoose.Schema({
     type: String,
     validate: {
       validator: function (v) {
-        return /^\d{2,3}-\d{6,}$/.test(v);
+        return /^\d{2,3}-\d{6,}$/.test(v)
       },
       message: (props) => `${props.value} is not valid unfortunately`,
     },
     minLength: 8,
   },
-});
+})
 
-entrySchema.set("toJSON", {
+entrySchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Entry", entrySchema);
+module.exports = mongoose.model('Entry', entrySchema)
